f73508fd4b32fdd37f399c9a184de54e647daa32
MuhammadWahajKhan <mwk.pk15@gmail.com>
2023-01-20 21:31:41 +0500

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,29 @@
+const { default: axios } = require('axios');
+
+require('./bootstrap');
+
+const form = document.getElementById('form');
+const inputMessage = document.getElementById('input-message');
+const listMessage = document.getElementById('list-messages');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const userInput = inputMessage.value;
+    axios.post('/chat-message', {
+        message: userInput
+    })
+})
+
+const channel = Echo.channel('public.chat.1');
+
+
+channel.subscribed( ()=> {
+    console.log("Subscribed");
+}).listen('.chat-message', (event)=> {
+    console.log(event);
+    const message = event.message;
+
+    const li = document.createElement('li');
+    li.textContent = message;
+
+    listMessage.appendChild(li);
+})
